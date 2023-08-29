2eda06671e2d7d65f36388225e92e30ee07b110e
Mahnoor Fatima <fatimamahnoor22@gmail.com>
2023-08-29 13:43:32 +0300

    Second Commit

--- MathTest.py
+++ MathTest.py
@@ -2,5 +2,5 @@ acceleration_due_to_gravity = float(input("Enter your value of acceleration in m
 mass_of_object = float(input("Enter mass of object in Kg: "))
 formulae_of_weight = acceleration_due_to_gravity * mass_of_object
 final_answer = str(round(formulae_of_weight, 2))
-final_form = "Your total weight rounded to 2 decimal places is " + str(final_answer)
+final_form = "Your total weight rounded to 2 decimal places is " + final_answer
 print(final_form)
